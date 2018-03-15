ddf4488ff354355ca6a3cce132743d0dee250666
hector ariceaga <hector_ariceaga@hotmail.com>
2018-03-15 16:25:19 +0000

    automatically backed up by learn

---
+++ index.js
@@ -0,0 +1,11 @@
+function iterativeLog(array){
+array.forEach(function(element,index) {
+  console.log(`${index}: ${element}`)
+})
+}
+
+function iterate(callback){
+  var stuff = [1,2,3]
+  stuff.forEach(callback)
+  return stuff
+}
